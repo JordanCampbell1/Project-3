2b3139a6e64212de570f4ccf7508243633b2b07f
Kyval Waysome <waysomekyval@gmail.com>
2023-04-15 23:45:09 -0700

    tajae and kyval part for notification panel and pop panle sound

--- NotificationPanel.java
+++ NotificationPanel.java
@@ -1,3 +1,38 @@
-public class NotificationPanel {
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class NotificationPanel extends JFrame{
+
+    private JPanel p = new JPanel();
+    private JLabel messageLabel = new JLabel("New task created!");
+
+    public NotificationPanel() {
+        setTitle("Enable Notification");
+        setResizable(false);
+        setSize(200, 100);
+
+        p.add(messageLabel);
+
+        getContentPane().add(p);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        playSound("mixkit-interface-option-select-2573.wav");
+    }
     
+    private void playSound(String soundFilePath) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFilePath)));
+            clip.start();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
+
+    
--- PopUpPanel.java
+++ PopUpPanel.java
@@ -1,4 +1,10 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,17 +20,31 @@ public class PopUpPanel extends JFrame{
     {
         setTitle("Issue With New Task Entry");
         setResizable(false);
-        setSize(300, 400);
+        setSize(400, 500);
 
         p.setBackground(Color.RED);
 
         p.add(tLabel);
+        tLabel.setFont(new Font("Arial", Font.BOLD, 20));
 
         getContentPane().add(p);
+        setLocationRelativeTo(null); // Center the frame on the screen
+        
+        PopUpPanel.playSound("Discord notification - Pop Up beep [sound effect] (3).mp3");
 
         pack();
 
         setVisible(true);
+        
+    }private static void playSound(String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(soundFile));
+            clip.start();
+        } catch (Exception e) {
+            System.err.println("Error playing sound: " + e.getMessage());
+        }
     }
     
 }
